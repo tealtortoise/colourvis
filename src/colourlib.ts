44e8f8c3adad999c73d9f53f9607669769bae5fe
Thea Berry <thea@mulberrymaple.com>
2022-04-26 21:12:44 +0100

    Refactor

--- src/colourlib.ts
+++ src/colourlib.ts
@@ -187,12 +187,6 @@ class ColourXYZ extends ThreeDimColourFormat {
         return rgb
     }
 
-    // setxyY(x: number, y: number, Y: number): void {
-    // this.d1 = (x * Y) / y
-    // this.d2 = Y
-    // this.d3 = ((1 - x - y) * Y) / y
-    // }
-
     getxyY(): xyY {
         const sum = this.d1 + this.d2 + this.d3
         const x = this.d1 / sum
@@ -200,74 +194,6 @@ class ColourXYZ extends ThreeDimColourFormat {
         const Y = this.d2
         return new xyY(x, y, Y)
     }
-
-    setsRGB(R: number, G: number, B: number) {
-        const aa = 12.92
-        const bb = 1.055
-        const cc = 0.055
-        const ee = 0.04045
-        const matrix = [
-            [0.4124564, 0.3575761, 0.1804375],
-            [0.2126729, 0.7151522, 0.072175],
-            [0.0193339, 0.119192, 0.9503041],
-        ]
-
-        const RGB = [R, G, B]
-        const RGBScaled = RGB.map((n: number) => {
-            let linearised = 0
-            if (n > ee) {
-                linearised = Math.pow((n + cc) / bb, 2.4)
-            } else {
-                linearised = n / aa
-            }
-            return linearised
-        })
-        const XYZ = dotProduct(RGBScaled, matrix)
-        // debugger
-        this.d1 = XYZ[0]
-        this.d2 = XYZ[1]
-        this.d3 = XYZ[2]
-    }
-
-    // setYuv(Y: number, u: number, v: number): void {
-    //     const x = (3 * u) / (2 * u - 8 * v + 4)
-    //     const y = (2 * v) / (2 * u - 8 * v + 4)
-    //     this.setxyY(x, y, Y)
-    // }
-
-    // setDaylightCCT(cct: number, Y = 1): void {
-    //     if (cct < 4000) {
-    //         throw new RangeError("Daylight CCT must be > 4000K")
-    //     } else if (4000 <= cct && cct <= 7000) {
-    //         var term1 = -4.607e9 / Math.pow(cct, 3)
-    //         var term2 = 2.9678e6 / Math.pow(cct, 2)
-    //         var term3 = 0.09911e3 / cct
-    //         var term4 = 0.244063
-    //         var x = term1 + term2 + term3 + term4
-    //     } else if (7000 < cct && cct <= 25000) {
-    //         var term1 = -2.0064e9 / Math.pow(cct, 3)
-    //         var term2 = 1.9018e6 / Math.pow(cct, 2)
-    //         var term3 = 0.24748e3 / cct
-    //         var term4 = 0.23704
-    //         var x = term1 + term2 + term3 + term4
-    //     } else {
-    //         throw new RangeError("Daylight CCT must be <= 25000K")
-    //     }
-
-    //     const y = -3 * Math.pow(x, 2) + 2.87 * x - 0.275
-    //     this.setxyY(x, y, Y)
-    // }
-
-    // setBlackbodyCCT(cct: number, Y = 1) {
-    //     const cct2 = Math.pow(cct, 2)
-    //     const u =
-    //         (0.860117757 + 1.54118254e-4 * cct + 1.28641212e-7 * cct2) /
-    //         (1 + 8.42420235e-4 * cct + 7.08145163e-7 * cct2)
-    //     const v =
-    //         (0.317398726 + 4.22806245e-5 * cct + 4.20481691e-8 * cct2) /
-    //         (1 - 2.89741816e-5 * cct + 1.61456053e-7 * cct2)
-    //     this.setYuv(Y, u, v)
-    // }
 
     get X() {
         return this.d1
@@ -278,27 +204,20 @@ class ColourXYZ extends ThreeDimColourFormat {
     get Z() {
         return this.d3
     }
-
-    blend(other: ColourXYZ, prop: number = 0.5) {
-        const neg = 1 - prop
-        this.d1 = prop * other.d1 + neg * this.d1
-        this.d2 = prop * other.d2 + neg * this.d2
-        this.d3 = prop * other.d3 + neg * this.d3
-    }
 }
 
-function newFromxyY(inxyY: xyY): ColourXYZ {
+function newFrom_xyY(inxyY: xyY): ColourXYZ {
     const { x, y, Y } = inxyY
     const X = (x * Y) / y
     const Z = ((1 - x - y) * Y) / y
     return new ColourXYZ(X, Y, Z)
 }
 
-function newFromYuv(input: Yuv): ColourXYZ {
+function newFrom_Yuv(input: Yuv): ColourXYZ {
     const { Y, u, v } = input
     const x = (3 * u) / (2 * u - 8 * v + 4)
     const y = (2 * v) / (2 * u - 8 * v + 4)
-    return newFromxyY(new xyY(x, y, Y))
+    return newFrom_xyY(new xyY(x, y, Y))
 }
 function newFrom_sRGB(input: RGB): ColourXYZ {
     const aa = 12.92
@@ -325,73 +244,85 @@ function newFrom_sRGB(input: RGB): ColourXYZ {
     return new ColourXYZ(...xyz)
 }
 
-function newFromBlackbodyCCT(cct: number, Y = 1) {
-    const cct2 = Math.pow(cct, 2)
-    const u =
-        (0.860117757 + 1.54118254e-4 * cct + 1.28641212e-7 * cct2) /
-        (1 + 8.42420235e-4 * cct + 7.08145163e-7 * cct2)
-    const v =
-        (0.317398726 + 4.22806245e-5 * cct + 4.20481691e-8 * cct2) /
-        (1 - 2.89741816e-5 * cct + 1.61456053e-7 * cct2)
-    return newFromYuv(new Yuv(Y, u, v))
+type CCTFactory = {
+    (cctK: number, Y?: number): ColourXYZ
+} & {
+    minK: number
+    maxK: number
 }
+const newBlackbody = <CCTFactory>Object.assign(
+    function newFromBlackbodyCCT(cctK: number, Y = 1) {
+        const cct2 = Math.pow(cctK, 2)
+        const u =
+            (0.860117757 + 1.54118254e-4 * cctK + 1.28641212e-7 * cct2) /
+            (1 + 8.42420235e-4 * cctK + 7.08145163e-7 * cct2)
+        const v =
+            (0.317398726 + 4.22806245e-5 * cctK + 4.20481691e-8 * cct2) /
+            (1 - 2.89741816e-5 * cctK + 1.61456053e-7 * cct2)
+        return newFrom_Yuv(new Yuv(Y, u, v))
+    },
+    { minK: 1000, maxK: 25000 }
+)
 
-function newFromDaylightCCT(cct: number, Y = 1) {
-    if (cct < 4000) {
-        throw new RangeError("Daylight CCT must be > 4000K")
-    } else if (4000 <= cct && cct <= 7000) {
-        var term1 = -4.607e9 / Math.pow(cct, 3)
-        var term2 = 2.9678e6 / Math.pow(cct, 2)
-        var term3 = 0.09911e3 / cct
-        var term4 = 0.244063
-        var x = term1 + term2 + term3 + term4
-    } else if (7000 < cct && cct <= 25000) {
-        var term1 = -2.0064e9 / Math.pow(cct, 3)
-        var term2 = 1.9018e6 / Math.pow(cct, 2)
-        var term3 = 0.24748e3 / cct
-        var term4 = 0.23704
-        var x = term1 + term2 + term3 + term4
-    } else {
-        throw new RangeError("Daylight CCT must be <= 25000K")
-    }
+const newDaylight = <CCTFactory>Object.assign(
+    function (cctK: number, Y = 1) {
+        let x: number
+        if (cctK < 4000) {
+            throw new RangeError("Daylight CCT must be >= 4000K")
+        } else if (4000 <= cctK && cctK <= 7000) {
+            x =
+                -4.607e9 / Math.pow(cctK, 3) +
+                2.9678e6 / Math.pow(cctK, 2) +
+                0.09911e3 / cctK +
+                0.244063
+        } else if (7000 < cctK && cctK <= 25000) {
+            x =
+                -2.0064e9 / Math.pow(cctK, 3) +
+                1.9018e6 / Math.pow(cctK, 2) +
+                0.24748e3 / cctK +
+                0.23704
+        } else {
+            throw new RangeError("Daylight CCT must be <= 25000K")
+        }
 
-    const y = -3 * Math.pow(x, 2) + 2.87 * x - 0.275
-    return newFromxyY(new xyY(x, y, Y))
-}
-
-// class sRGB extends ThreeDimColourFormat {
-//     r = this.d1
-//     g = this.d2
-//     b = this.d3
-// }
-
-// function xyYtosRGB(input: xyY): sRGB {
-//     return new sRGB(0, 0, 0)
-// }
+        const y = -3 * Math.pow(x, 2) + 2.87 * x - 0.275
+        return newFrom_xyY(new xyY(x, y, Y))
+    },
+    { minK: 4000, maxK: 25000 }
+)
 
 const D65 = new ColourXYZ(95.047 / 100, 1, 108.883 / 100)
 const D50 = new ColourXYZ(0.9642, 1, 0.8249)
 
-function getSpectral(nm: number): ColourXYZ {
-    const intnm = Math.round(nm)
-    return new ColourXYZ(
-        CIE1931CMF_X[intnm - CIE1931CMF_StartNM],
-        CIE1931CMF_Y[intnm - CIE1931CMF_StartNM],
-        CIE1931CMF_Z[intnm - CIE1931CMF_StartNM]
-    )
+type SpectralFactory = {
+    (nm: number): ColourXYZ
+} & {
+    min: number
+    max: number
 }
+const newSpectralNM = <SpectralFactory>Object.assign(
+    function (nm: number): ColourXYZ {
+        const intnm = Math.round(nm)
+        return new ColourXYZ(
+            CIE1931CMF_X[intnm - CIE1931CMF_StartNM],
+            CIE1931CMF_Y[intnm - CIE1931CMF_StartNM],
+            CIE1931CMF_Z[intnm - CIE1931CMF_StartNM]
+        )
+    },
+    { min: CIE1931CMF_StartNM, max: CIE1931CMF_FinishNM }
+)
 
 export {
     ColourXYZ,
     D65,
     D50,
-    getSpectral,
+    newSpectralNM,
     xyY,
     clamp,
-    newFromxyY,
+    newFrom_xyY,
     RGB,
     newFrom_sRGB,
-    newFromBlackbodyCCT,
-    newFromDaylightCCT,
+    newBlackbody,
+    newDaylight,
 }
 // export type { RGB }
